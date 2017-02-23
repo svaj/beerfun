4d60be8abdc4cf01a4b7e0e701701f54f6e80390
Chris Svajlenka <chris@svajlenka.com>
2017-02-23 16:42:22 -0500

    Work towards getting product listing on a category page.

--- routes/category.js
+++ routes/category.js
@@ -12,11 +12,7 @@ router.get('/:categoryId', function(req, res, next) {
                                       .where(`slug(en="${req.params.categoryId}")`)
                                       .perPage(25)
                                       .build({ projectKey: projectKey });
-  const productUri = requestBuilder.products
-                                      .where(`masterData(current(slug(en="${req.params.productId}")))`)
-                                      .sort('lastModifiedAt', false)
-                                      .perPage(15)
-                                      .build({ projectKey: projectKey });
+
 
     const request = {
       uri: categoryUri,
@@ -25,12 +21,17 @@ router.get('/:categoryId', function(req, res, next) {
     client.execute(request)
       .then(result => result.body.results)
       .then(cats => {
-        console.log(cats);
         if (cats.length) {
           const cat = cats[0];
-          // Get products in category
-          const prods = [];
-          res.render('category', { title: cat.name.en, cat: cat, products: prods });
+          const productUri = requestBuilder.products
+                                           .where(`masterData(current(categories(id="${cat.id}")))`)
+                                           .sort('lastModifiedAt', false)
+                                           .perPage(15)
+                                           .build({ projectKey: projectKey });
+          const prodRequest = { uri:productUri, method:'GET'};
+          client.execute(prodRequest)
+            .then(result => result.body.results)
+            .then(prods => { console.log(prods); res.render('category', { title: cat.name.en, cat: cat, products: prods }); });
         } else {
           next();
         }
